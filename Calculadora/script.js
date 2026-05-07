cb79aa97286582b8e3a9cc9ee4477b0ad903d297
Raffaelnss <santos.raffael@escola.pr.gov.br>
2026-05-07 19:42:20 +0000

    t7ygifu

---
+++ Calculadora/script.js
@@ -0,0 +1,11 @@
+let calculadora = {
+    ASCII: `█▀▀ ▄▀█ █░░ █▀▀ █░█ █░░ ▄▀█ █▀▄ █▀█ █▀█ ▄▀█
+█▄▄ █▀█ █▄▄ █▄▄ █▄█ █▄▄ █▀█ █▄▀ █▄█ █▀▄ █▀█`,
+    OperadorText: "Qual operador você escolhe?",
+    Numero1Text: "Digite o primeiro número:",
+    Numero2Text: "Digite o segundo número:",
+
+}
+
+console.log(calculadora.ASCII, "\n\n");
+let a = prompt()
